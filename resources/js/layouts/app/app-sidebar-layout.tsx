13f0e5d0fa39b1d3533699db326a310d32728e2a
Maulana <muammar676@gmail.com>
2026-07-03 01:16:49 +0700

    fix layout

--- resources/js/layouts/app/app-sidebar-layout.tsx
+++ resources/js/layouts/app/app-sidebar-layout.tsx
@@ -13,7 +13,9 @@ export default function AppSidebarLayout({
             <AppSidebar />
             <AppContent variant="sidebar" className="overflow-x-hidden">
                 <AppSidebarHeader breadcrumbs={breadcrumbs} />
+                <div className='p-4'>
                 {children}
+                </div>
             </AppContent>
         </AppShell>
     );
